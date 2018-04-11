829ed6dcc5e7f522399bf527cf8940aa0c26dc5a
Kai Yang <kai.yang412@gmail.com>
2018-04-11 01:40:10 -0400

    Better column highlighting

--- .emacs.d/lisp/defuns-config.el
+++ .emacs.d/lisp/defuns-config.el
@@ -20,17 +20,4 @@ http://www.emacswiki.org/emacs/BackwardDeleteWord
   (interactive "p")
   (delete-word (- arg)))
 
-(setq show-column-marker 'nil)
-(defun column-mark-toggle ()
-  "Toggle marker for column 120"
-  (interactive)
-  (if show-column-marker
-      (progn
-        (message "column 120 marker OFF")
-        (column-marker-1 -2))
-    (progn
-      (message "column 120 marker ON")
-      (column-marker-1 120)))
-  (setq show-column-marker (not show-column-marker)))
-
 (fset 'align-equals "\C-[xalign-regex\C-m=\C-m")
