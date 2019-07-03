814d92525cec0a777233393594507b7586fb3d15
Kai Yang <kai.yang412@gmail.com>
2019-07-02 21:50:50 -0400

    Add lsp-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,9 +23,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("/Users/kaiyang/Dropbox/org/fitness.org" "/Users/kaiyang/Dropbox/org/journal.org" "/Users/kaiyang/Dropbox/org/interests.org" "/Users/kaiyang/Dropbox/org/stuff.org" "/Users/kaiyang/org/refile.org" "/Users/kaiyang/org/stuff.org" "/Users/kaiyang/org/journal.org" "/Users/kaiyang/org/tasks.org")))
  '(package-selected-packages
    (quote
-    (company-lsp flycheck lsp-ui ein lsp-mode git-gutter dumb-jump ace-window which-key use-package undo-tree smooth-scrolling neotree magit iedit hungry-delete hlinum expand-region doom-themes doom-modeline counsel-projectile avy))))
+    (protobuf-mode typescript-mode bazel-mode docker-compose-mode dockerfile-mode use-package company-lsp lsp-ui lsp-mode git-gutter doom-themes doom-modeline))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
