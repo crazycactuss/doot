d63008bf672ef162a97622c05e6767d1aceef47d
Kai Yang <kai.yang412@gmail.com>
2022-01-24 03:30:36 -0800

    emacs: overhaul

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,13 +1,9 @@
 ;; --------------------
 ;; -- MELPA Settings --
 ;; --------------------
-;; initialize MELPA
-(require 'package)
-(setq package-enable-at-startup nil)
 ;; specify MELPA repo
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -18,3 +14,20 @@
 ;; --------------------
 ;; load configuration from config.org
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
+ '(highlight-indentation-face ((t (:background "gray25"))))
+ '(org-document-title ((t (:foreground "#c678dd" :weight bold :height 2.0))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
+ '(org-level-2 ((t (:inherit outline-2 :extend nil :height 1.25))))
+ '(org-level-3 ((t (:inherit outline-3 :extend nil :height 1.1)))))
